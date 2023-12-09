f264a235064564bb1a3cfb94fb4bf063e45fa03c
super-SH <dev.swam.htet@gmail.com>
2023-12-09 18:44:39 +0630

    file and folder structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Movies, MovieDetails, Actors, NavBar, Profile } from './';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <main>
+        <Routes>
+          <Route index element={<Navigate to='/movies' replace />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/movie/movieId' element={<MovieDetails />} />
+          <Route path='/actors/:actorId' element={<Actors />} />
+          <Route path='/profile/:id' element={<Profile />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
